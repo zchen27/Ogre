14b55dad74d246a035045d432bd9be687dae424a
zchen27 <zchen27@gmail.com>
2014-04-02 13:59:17 -0400

    Stuff. Including Weapon and MainBattery

---
+++ src/Weapon.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zchen0704
+ */
+public abstract class Weapon
+{	
+	protected int defense;
+	
+	protected int attack;
+	
+	protected int range;
+	
+	private Status status;
+	
+	private boolean reloading;
+}
