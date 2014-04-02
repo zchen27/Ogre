9fc27f72199c87025134fc55165786e86e65907c
xLuke <ljdittman@gmail.com>
2014-04-02 13:43:53 -0400

    Started unit

---
+++ src/Unit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ldittm0798
+ */
+public abstract class Unit {
+    
+    int cost;
+    int movement;
+    Location loc;
+    Status status;
+    
+    public void die(){
+        
+    }
+    
+    void placeSelfOnGrid(Location loc){
+        
+    }
+    
+}
