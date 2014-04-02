41eb07c92621dd51fe2c34181f47d5e319d29d26
xLuke <ljdittman@gmail.com>
2014-04-02 14:09:25 -0400

    Made all armor units

---
+++ src/Armor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ldittm0798
+ */
+public abstract class Armor extends Unit {
+    
+    int attack;
+    int range;
+    int defense;
+    
+    void disable(){
+        
+    }
+    
+    void attack(Location loc){
+        
+    }
+    
+}
---
+++ src/MobileHowitzer.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ldittm0798
+ */
+public class MobileHowitzer {
+    
+}
