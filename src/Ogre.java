f9a9ba5f3e9053c4618a56a6018befb36f10b076
xLuke <ljdittman@gmail.com>
2014-04-03 14:07:55 -0400

    Updates to ogre methods

--- src/Ogre.java
+++ src/Ogre.java
@@ -14,80 +14,95 @@ public abstract class Ogre extends Unit {
     
     protected ArrayList<MainBattery> main;
     protected ArrayList<SecondaryBattery> secondary;
-    protected ArrayList<MissileTube> rack;
+    protected ArrayList<MissileTube> tube;
     protected ArrayList<ExternalMissile> external;
     protected ArrayList<Antipersonnel> ap;
     
-    int internalMissile;
+    int internalMissiles;
+    int tracks;
     String className;
     String unitName;
     
     public int getTracks(){
-        
+        return tracks;
     }
     
     public int getMainBattery(){
-        
+        return main.size();
     }
     
     public int getSecondaryBattery(){
-        
+        return secondary.size();
     }
     
     public int getMissileTube(){
-        
+        return tube.size();
     }
     
     public int getRemainingInternalMissiles(){
-        
+        return internalMissiles;
     }
     
     public void mainBatteryAttack(Location loc){
-        
+        //
     }
     
     public void secondaryBatteryAttack(Location loc){
-        
+        //
     }
     
     public void externaMissileAttack(Location loc){
-        
+        //
     }
     
     public void internalMissileAttack(Location loc){
-        
+        //
     }
     
     public void apAttack(Location loc){
-        
+        //
     }
     
     public void overrun(Location loc){
-        
+        //
     }
     
-    public void hitMainBattery(){
-        
+    public void killMainBattery(){
+        main.remove(0);
     }
     
-    public void hitSecondaryBattery(){
-        
+    public void killSecondaryBattery(){
+        secondary.remove(0);
     }
     
-    public void hitTracks(){
-        
+    public void killTracks(int num){
+        tracks = tracks-num;
+    }
+    
+    public void killMissileTube(){
+        tube.remove(0);
+    }
+    
+    public void killExternalMissile(){
+        external.remove(0);
+    }
+    
+    public void killAntipersonnel(){
+        ap.remove(0);
     }
     
     @Override public void move(Location loc){
-        
+        //
     }
     
     @Override public void die(){
-        
+        if((main.size()==0)&&(secondary.size()==0)&&(tube.size()==0)&&(external.size()==0)&&(ap.size()==0)&&(tracks==0)){
+            status = Status.DEAD;
+        }
     }
     
     public void lastResult(){
-        
+        //
     }
     
 }
