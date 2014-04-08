8f3dd704f7dffaa41114b0c2b24f50ea44a50102
xLuke <ljdittman@gmail.com>
2014-04-08 14:02:27 -0400

    getNeighbors for Location, still editing Unit movement

--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+
+
 
 /*
  * To change this template, choose Tools | Templates
@@ -85,6 +88,17 @@ public class Location implements Comparable
 				return null;
 		}
 	}
+        
+        public ArrayList<Location> getNeighbors(){
+            ArrayList<Location> neighbors = new ArrayList<Location>();
+            neighbors.add(getAdjacentLocation(Direction.N));
+            neighbors.add(getAdjacentLocation(Direction.NE));
+            neighbors.add(getAdjacentLocation(Direction.NW));
+            neighbors.add(getAdjacentLocation(Direction.S));
+            neighbors.add(getAdjacentLocation(Direction.SE));
+            neighbors.add(getAdjacentLocation(Direction.SW));
+            return neighbors;
+        }
 	
 	public void setCrater(boolean b)
 	{
--- src/Unit.java
+++ src/Unit.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -30,11 +35,34 @@ public abstract class Unit {
         //
     }
     
-    public boolean checkLocation(Location loc){
-        if(loc.isCrater() || loc.){
+    public boolean checkLocation(Location loc, Game game){
+        if(loc.isCrater() || game.scanLocations(loc)){
             return false;
         }
         return true;
+        //returns true if location is empty
+    }
+    
+    public ArrayList<Location> availableMoves(Game game){
+        //Check locations extending out from "loc" a number of "movement" steps
+        //Remove duplicates (java.util.set)
+        //NEED getNeighbors METHOD!!!
+        ArrayList<Location> fringes = new ArrayList<Location>();
+        fringes.add(loc);
+        //...http://www.redblobgames.com/grids/hexagons/
+        for(int k=1; k<=movement; k++){
+            ArrayList<Location> lastSet = new ArrayList<Location>();
+            lastSet = loc.getNeighbors();
+            for(int j=0; j<lastSet.size(); j++){
+                boolean check1 = checkLocation(lastSet.get(j), game);
+                if(!check1){
+                    lastSet.remove(j);
+                    j--;
+                }
+            }
+        }
+        Set<Location> fringeSet = new HashSet<Location>(fringes);
+        fringes = new ArrayList<Location>(fringeSet);
     }
     
 }
