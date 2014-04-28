3529df8c69174b64cd9baf3abe5cc6d6927d9785
zchen27 <zchen27@gmail.com>
2014-04-28 02:00:48 -0400

    Commiting Up.
    Made something about drawing hexagons and roughly identifying them. The
    game is still a bit incomplete and I have no idea what to do with it.

--- src/OgreWindow.java
+++ src/OgreWindow.java
@@ -1,11 +1,15 @@
 
+import java.awt.Graphics;
+import java.awt.Polygon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 
 /*
@@ -21,8 +25,60 @@ public class OgreWindow implements ActionListener, MouseListener
 {
 	private JFrame frame;
 	private JPanel boardPanel;
+	private JPopupMenu popup;
 	private Game game;
-
+	private double radius;
+	private double width;
+	private double side;
+	private double height;
+	private Location[] sel = new Location[2];
+	
+	private class OgrePanel extends JPanel
+	{
+		@Override
+		public void paintComponents(Graphics g)
+		{
+			super.paintComponents(g);
+			int[] xpoints = new int[6];
+			int[] ypoints = new int[6];
+			
+			DefaultGrid grid;
+			grid = (DefaultGrid) game.getGrid();
+			
+			for(int c = 0; c < grid.getNumCols(); c++)
+			{
+				for(int r = 0; r < grid.getNumRows(); r++)
+				{
+					int dx = c * (int) width;
+					int dy = r * (int) height;
+					
+					if(c % 2 != 0)
+					{
+						dy += height / 2;
+					}
+					
+					for(int i = 0; i < 6; i++)
+					{
+						xpoints[i] = (int) ((int) 25 * Math.cos(i * 2 * Math.PI / 6) + dx);
+						ypoints[i] = (int) ((int) 25 * Math.sin(i * 2 * Math.PI / 6) + dy);
+					}
+					
+					Polygon hex = new Polygon (xpoints, ypoints, 6);
+					g.drawPolygon(hex);
+				}
+			}
+			
+		}
+	}
+	
+	public OgreWindow(Game g, double r)
+	{
+		game = g;
+		radius = r;
+		side = radius * 3/2;
+		height = Math.sqrt(3) * radius;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent ae)
 	{
@@ -32,11 +88,64 @@ public class OgreWindow implements ActionListener, MouseListener
 	@Override
 	public void mouseClicked(MouseEvent me)
 	{
-		int x = me.getX();
-		int y = me.getY();
+		double x = me.getX();
+		double y = me.getY();
+		int button = me.getButton();
 		
-		Grid grid = game.getGrid();
 		
+		
+		Location loc = convert(x, y);
+		
+	}
+	
+	private Location convert(double x, double y)
+	{
+				
+		double xt;
+		double yt;
+		
+		double r;
+		double c;
+		
+		double rt;
+		double ct;
+		
+		double dr;
+		
+		ct = x / side;
+		if(ct % 2 == 0)
+		{
+			rt = y / height;
+		}
+		else
+		{
+			rt =  (y - ( height) / 2) / (height);
+		}
+		
+		xt = x - ct * side;
+		yt = y - rt * height;
+		
+		if(yt > (height /2))
+		{
+			dr = 1;
+		}
+		else
+		{
+			dr = 0;
+		}
+		
+		if(xt > (radius * Math.abs(.5 - yt/height)))
+		{
+			c = ct;
+			r = rt;
+		}
+		else
+		{
+			c = ct - 1;
+			r = rt - c%2 + dr;
+		}
+		
+		return new Location((int) c, (int) r);
 	}
 
 	@Override
