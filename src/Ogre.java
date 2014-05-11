ce62f894c91363e73eede5803ab25d8a97506233
zchen27 <zchen27@gmail.com>
2014-05-11 13:51:37 -0400

    Going to to a lot about Ogre.
    Movement and attack for normal unit is on you.

--- src/Ogre.java
+++ src/Ogre.java
@@ -11,98 +11,93 @@ import java.util.ArrayList;
  * @author ldittm0798
  */
 public abstract class Ogre extends Unit {
-    
-    protected ArrayList<MainBattery> main;
-    protected ArrayList<SecondaryBattery> secondary;
-    protected ArrayList<MissileTube> tube;
-    protected ArrayList<ExternalMissile> external;
-    protected ArrayList<Antipersonnel> ap;
-    
-    int internalMissiles;
-    int tracks;
-    String className;
-    String unitName;
-    
-    public int getTracks(){
-        return tracks;
-    }
-    
-    public int getMainBattery(){
-        return main.size();
-    }
-    
-    public int getSecondaryBattery(){
-        return secondary.size();
-    }
-    
-    public int getMissileTube(){
-        return tube.size();
-    }
-    
-    public int getRemainingInternalMissiles(){
-        return internalMissiles;
-    }
-    
-    public void mainBatteryAttack(Location loc){
-        //
-    }
-    
-    public void secondaryBatteryAttack(Location loc){
-        //
-    }
-    
-    public void externaMissileAttack(Location loc){
-        //
-    }
-    
-    public void internalMissileAttack(Location loc){
-        //
-    }
-    
-    public void apAttack(Location loc){
-        //
-    }
-    
-    public void overrun(Location loc){
-        //
-    }
-    
-    public void killMainBattery(){
-        main.remove(0);
-    }
-    
-    public void killSecondaryBattery(){
-        secondary.remove(0);
-    }
-    
-    public void killTracks(int num){
-        tracks = tracks-num;
-    }
-    
-    public void killMissileTube(){
-        tube.remove(0);
-    }
-    
-    public void killExternalMissile(){
-        external.remove(0);
-    }
-    
-    public void killAntipersonnel(){
-        ap.remove(0);
-    }
-    
-    @Override public void move(Location loc){
-        //
-    }
-    
-    @Override public void die(){
-        if((main.size()==0)&&(secondary.size()==0)&&(tube.size()==0)&&(external.size()==0)&&(ap.size()==0)&&(tracks==0)){
-            status = Status.DEAD;
-        }
-    }
-    
-    public void lastResult(){
-        //
-    }
-    
+
+	String className;
+	String unitName;
+
+	public abstract int getTracks();
+
+	public abstract int getMainBattery();
+	
+	public abstract int getSecondaryBattery();
+	
+	public abstract int getMissileTube();
+	
+	public abstract int getInternalMissiles();
+	
+	public abstract int setTracks(int n);
+	
+	public abstract int setMainBattery(int n);
+	
+	public abstract int setSecondaryBattery(int n);
+	
+	public abstract int setMissileTube(int n);
+	
+	public abstract int setInternalMissiles(int n);
+	
+	public void mainBatteryAttack(Location loc)
+	{
+		//
+	}
+
+	public void secondaryBatteryAttack(Location loc)
+	{
+		//
+	}
+
+	public void externaMissileAttack(Location loc)
+	{
+		//
+	}
+
+	public void internalMissileAttack(Location loc)
+	{
+		//
+	}
+	
+	public void overrun(Location loc)
+	{
+		//
+	}
+	
+	public void killMainBattery()
+	{
+		
+	}
+
+	public void killSecondaryBattery()
+	{
+		
+	}
+
+	public void killTracks(int num)
+	{
+		
+	}
+
+	public void killMissileTube()
+	{
+		
+	}
+
+	public void killExternalMissile()
+	{
+		
+	}
+
+
+	@Override public void move(Location loc)
+	{
+		//
+	}
+
+	@Override public void die()
+	{
+		
+	}
+
+	public void lastResult(){
+	//
+	}
+
 }
